a83187428d4b8a6a682ddb7759775f3ab8572b68
nem0o <adam0smit@mail.ru>
2022-05-24 15:16:31 +0600

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ColumnsModule } from './columns/columns.module';
+import { AuthModule } from './auth/auth.module';
+import { CardsModule } from './cards/cards.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentsModule } from './comments/comments.module';
+
+@Module({
+  imports: [
+    CommentsModule,
+    UsersModule,
+    ColumnsModule,
+    CardsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: 5400,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+      host: 'localhost',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
